d803a2ea02951a375aecb4a2a9cc90b7cd083165
Bharath S J <166963664+Bharath-S-J@users.noreply.github.com>
2025-05-07 15:16:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 var isDate = function (input) {
   //   write your code here
+	if (Object.prototype.toString.call(input) === "[object Date]" && !isNaN(input.getTime())) {
+        return true;
+    }
+
+    // Try to parse if it's not already a Date object
+    const parsed = new Date(input);
+    return !isNaN(parsed.getTime());
 };
 
 // Do not change the code below.
